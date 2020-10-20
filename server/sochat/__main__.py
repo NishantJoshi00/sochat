25837d5276a8feeef856177056d66edba846960f
Nishant Joshi <nishantjo.12@gmail.com>
2020-10-20 11:12:17 +0530

    Engine and transactions

--- server/sochat/__main__.py
+++ server/sochat/__main__.py
@@ -1,12 +1,39 @@
 import socketserver
 import threading
+import random
 import sys
+from .engine import Chat_Engine, User
+import os
+
+runner = Chat_Engine();
+with os.popen("openssl prime -generate -bits 16") as f:
+	P = int(f.read())
+with os.popen("openssl prime -generate -bits 8") as f:
+	Q = int(f.read())
 class Handler(socketserver.BaseRequestHandler):
 	def handle(self):
-		user = self.handshake()
-		user.run(self.request)
+		token = self.handshake()
+		user = self.get_user()
+		user.run()
 	def handshake(self):
+		_t = self.request.recv(1)
+		# 1. Sending the public key to client
+		self.request.sendall(P.to_bytes(2, byteorder='little'))
+		self.request.sendall(Q.to_bytes(1, byteorder='little'))
+		with os.popen("openssl prime -generate -bits 8") as f:
+			_token = int(f.read())
+		send_key = (Q ** _token) % P
+		self.request.sendall(send_key.to_bytes(2, byteorder='little'))
+		recieve_key = int.from_bytes(self.request.recv(2), 'little')
+		_token = (recieve_key ** _token) % P
+		return _token
+	def get_user(self):
 		...
+
+
+		
+		
+		
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
 	pass
--- server/sochat/engine.py
+++ server/sochat/engine.py
@@ -1,6 +1,39 @@
 # Import Section
 import logging
+from werkzeug.security import generate_password_hash, check_password_hash
 # 
+
+class User:
+	def __init__(self, username, password):
+		self._username = username
+		self._password = generate_password_hash(password)
+		self._token = None
+		self._buffer = None
+	def set_token(self, token):
+		self._token = token
+	def put_message(self, message):
+		self._buffer = message
+	def flush(self):
+		_ = self._buffer
+		self._buffer = None
+		return _
+
+
+class Room:
+	def __init__(self, name, admin):
+		self.name = name
+		self._admin = admin
+		self._members = [admin]
+	def add_member(self, user):
+		self._members.append(user)
+	def remove_member(self, user):
+		self._members.remove(user)
+	def reassign_admin(self, user):
+		self._admin = user
+		self._members.remove(user)
+		self._members.insert(0, user)
+
+
 class Chat_Engine():
 	def __init__(self, port=None, user_limit=100, room_limit=100):
 		self.configuration = {
@@ -16,9 +49,13 @@ class Chat_Engine():
 		user.connect(self)
 	def remove_user(self, user):
 		self._users.remove(user)
-	def add_room(self, room):
-		self._rooms.append(room)
-	def remove_room(self, room):
+	def add_room(self, room: Room):
+		if room.name not in [i.name for i in self._rooms]:
+			self._rooms.append(room)
+			return 0
+		else:
+			return -1
+	def remove_room(self, room: Room):
 		self._rooms.remove(room)
 	def dump(self):
 		data = {
@@ -30,6 +67,10 @@ class Chat_Engine():
 	def load(self, data):
 		self.configuration = data['info']
 		self._rooms, self._users = data['rooms'], data['users']
+
+
+
+	
 
 
 
